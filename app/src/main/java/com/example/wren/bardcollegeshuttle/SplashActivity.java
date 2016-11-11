46ae5d9480e32f8b3da60a75a2f9c05c2f2b25db
wrenywren <wren.chance@gmail.com>
2016-11-11 11:30:17 -0500

    Added cool bottom to up animation for splash screen

--- app/src/main/java/com/example/wren/bardcollegeshuttle/SplashActivity.java
+++ app/src/main/java/com/example/wren/bardcollegeshuttle/SplashActivity.java
@@ -1,39 +1,72 @@
 package com.example.wren.bardcollegeshuttle;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.PixelFormat;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 /**
  * Created by Wren on 10/14/2016.
  */
 
-public class SplashActivity extends AppCompatActivity {
-
+public class SplashActivity extends Activity {
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        Window window = getWindow();
+        window.setFormat(PixelFormat.RGBA_8888);
+    }
+    /** Called when the activity is first created. */
+    Thread splashTread;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Thread timer = new Thread()
-        {
+        setContentView(R.layout.splash_screeen);
+        StartAnimations();
+    }
+    private void StartAnimations() {
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.alpha);
+        anim.reset();
+        LinearLayout l=(LinearLayout) findViewById(R.id.lin_lay);
+        l.clearAnimation();
+        l.startAnimation(anim);
+
+        anim = AnimationUtils.loadAnimation(this, R.anim.translate);
+        anim.reset();
+        ImageView iv = (ImageView) findViewById(R.id.bard_splash);
+        iv.clearAnimation();
+        iv.startAnimation(anim);
+
+        splashTread = new Thread() {
+            @Override
             public void run() {
                 try {
-                    sleep(1000);
+                    int waited = 0;
+                    // Splash screen pause time
+                    while (waited < 3500) {
+                        sleep(100);
+                        waited += 100;
+                    }
+                    Intent intent = new Intent(SplashActivity.this,
+                            ShuttleSelectionMenu.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                    startActivity(intent);
+                    SplashActivity.this.finish();
                 } catch (InterruptedException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
+                    // do nothing
+                } finally {
+                    SplashActivity.this.finish();
                 }
-                finally
-                {
-                    Intent i = new Intent(SplashActivity.this,ShuttleSelectionMenu.class);
-                    finish();
-                    startActivity(i);
-                }
+
             }
         };
-        timer.start();
+        splashTread.start();
 
     }
 
-
-
 }
+
