19bab60f33ff176e74ca616802bad5400c3813c1
wrenywren <wren.chance@gmail.com>
2016-11-16 14:47:09 -0500

    Added more Google Map Markers for ShuttleMapsActivity

--- app/src/main/java/com/example/wren/bardcollegeshuttle/ShuttleMapsActivity.java
+++ app/src/main/java/com/example/wren/bardcollegeshuttle/ShuttleMapsActivity.java
@@ -39,7 +39,36 @@ public class ShuttleMapsActivity extends FragmentActivity implements OnMapReadyC
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        //Kline towards Hannafords Marker
+        LatLng klineTowardHannaford = new LatLng(42.022227, -73.908498);
+        mMap.addMarker(new MarkerOptions().position(klineTowardHannaford).title("Kline Stop Towards Hannafords"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(klineTowardHannaford));
+
+        //Kline towards Tivoli Marker
+        LatLng klineTowardTivoli = new LatLng(42.022669, -73.908263);
+        mMap.addMarker(new MarkerOptions().position(klineTowardTivoli).title("Kline Stop Towards Tivoli"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(klineTowardTivoli));
+
+        //Ward Gate towards Hannaford Marker
+        LatLng wardGateTowardHannafords = new LatLng(42.027007, -73.905934);
+        mMap.addMarker(new MarkerOptions().position(wardGateTowardHannafords).title("Ward Gate Stop Towards Hannafords"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(wardGateTowardHannafords));
+
+        //Robbins Marker
+        LatLng robbinsStop = new LatLng(42.029348, -73.904594);
+        mMap.addMarker(new MarkerOptions().position(robbinsStop).title("Robbins Stop"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(robbinsStop));
+
+        //Tivoli Marker
+        
+        //Hannafords Towards Tivoli Marker
+        LatLng hannafordsStop = new LatLng(41.979889, -73.880896);
+        mMap.addMarker(new MarkerOptions().position(hannafordsStop).title("Hannafords Stop Towards Tivoli"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(hannafordsStop));
+
+
     }
 }
