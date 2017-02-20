67e158adefa79349b4d9a2a9a7bbe9c2a94bc008
wrenywren <wren.chance@gmail.com>
2017-02-20 15:39:26 -0500

    Added function to display notification in Task Bar, Might want to add a way to open the app by clicking on the notification. And I still need to change the info displayed on the notification.

--- app/src/main/java/com/example/wren/bardcollegeshuttle/AlarmManagerBroadcastReceiver.java
+++ app/src/main/java/com/example/wren/bardcollegeshuttle/AlarmManagerBroadcastReceiver.java
@@ -1,22 +1,32 @@
 package com.example.wren.bardcollegeshuttle;
 
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
 import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.PowerManager;
+import android.os.SystemClock;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v7.app.NotificationCompat;
 import android.util.Log;
 import android.widget.Toast;
+
+import static android.R.attr.delay;
+
 /**
  * Created by chancewren on 2/16/17.
  */
@@ -24,6 +34,9 @@ import android.widget.Toast;
 public class AlarmManagerBroadcastReceiver extends BroadcastReceiver {
     private static String ONE_TIME = "onetime";
     private static String BUS_TIME = "busTime";
+
+    public static String NOTIFICATION_ID = "notification-id";
+    public static String NOTIFICATION = "notification";
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -35,6 +48,8 @@ public class AlarmManagerBroadcastReceiver extends BroadcastReceiver {
                 "AlarmManagerBroadcastReceiver.onReceive()");
 
         wakeLock1.acquire();
+
+        notificationAlert(context, intent);
 
         callAlertDialogBox(context, intent);
 
@@ -71,8 +86,42 @@ public class AlarmManagerBroadcastReceiver extends BroadcastReceiver {
 
         Ringtone ringtone = RingtoneManager.getRingtone(context, alarmUri);
         ringtone.play();
+    }
+
+
+    public void notificationAlert(Context context, Intent intent) {
+        //you might want to check what's inside the Intent
+        if(intent != null)//(intent.getStringExtra("myAction") != null &&
+               // intent.getStringExtra("myAction").equals("mDoNotify")){
+        {
+            NotificationManager mNotifyMgr =
+                    (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+            NotificationCompat.Builder mBuilder = (NotificationCompat.Builder) new NotificationCompat.Builder(context)
+                    .setSmallIcon(R.drawable.ic_launcher)
+                    //example for large icon
+                    .setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.mipmap.ic_launcher))
+                    .setContentTitle("Bard College Shuttle Alert")
+                    .setContentText("TEST NOTIFICATION!!!")
+                    .setOngoing(false)
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                    .setAutoCancel(true);
+            Intent i = new Intent(context, AlarmManagerBroadcastReceiver.class);
+            PendingIntent pendingIntent =
+                    PendingIntent.getActivity(context, 0, i, PendingIntent.FLAG_ONE_SHOT);
+
+            // example for blinking LED
+            mBuilder.setLights(0xFFb71c1c, 1000, 2000);
+            //mBuilder.setSound(yourSoundUri);
+            mBuilder.setContentIntent(pendingIntent);
+            mNotifyMgr.notify(12345, mBuilder.build());
+        }
 
     }
+
+
+
+
 
     /**
      * This is the function which is called on click of 'Set Alarm' Button. It
@@ -117,7 +166,7 @@ public class AlarmManagerBroadcastReceiver extends BroadcastReceiver {
 
         Log.d("AlarmManagersetOneTimeAlarm()::", "Alarm set for : " + calender.getTime());
 
-        alarmManager.set(AlarmManager.RTC, (calender.getTimeInMillis()), pendingIntent);
+        alarmManager.set(AlarmManager.RTC_WAKEUP, (calender.getTimeInMillis()), pendingIntent);
 
         return calender.getTime().toString();
 
